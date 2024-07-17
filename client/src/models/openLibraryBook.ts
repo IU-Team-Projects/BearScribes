a97837120366b7ee51724bafa85c4d2f1a0a3677
Artur <arturmuxutdinov@gmail.com>
2024-07-18 00:02:54 +0300

    impl(prettier) (#16)

--- client/src/models/openLibraryBook.ts
+++ client/src/models/openLibraryBook.ts
@@ -1,10 +1,10 @@
 interface OpenLibraryBook {
-    author_name: string[],
-    cover_i: number,
-    first_publish_year: number,
-    first_sentence: string[],
-    title: string,
-    cover_url: string
+    author_name: string[];
+    cover_i: number;
+    first_publish_year: number;
+    first_sentence: string[];
+    title: string;
+    cover_url: string;
 }
 
-export default OpenLibraryBook
+export default OpenLibraryBook;
