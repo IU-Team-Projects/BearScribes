f459c0555633bb494f4845c9729848ee9ce64b38
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2024-07-19 13:33:02 +0300

    feat: add SEO metadata to all pages

--- client/src/app/searchBooks/page.tsx
+++ client/src/app/searchBooks/page.tsx
@@ -1,6 +1,11 @@
-'use client';
-
 import { SearchBooksPage } from '@/pages/searchBooks';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Search Books - Bearscribes',
+    description: `Use Bearscribes' search feature to find books available for exchange. Search by title, author, or genre to discover new books and connect with other members. Start searching today!`,
+    keywords: `search books, Bearscribes search, find books, book exchange search, book lookup`,
+};
 
 const Page = () => {
     return <SearchBooksPage />;
